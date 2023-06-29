397c713edbbaf9f3cebdfa9c2e392e5781fd5bf0
baishali <roybaishali07@gmail.com>
2023-06-29 21:17:28 +0530

    initial changes

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 //const mongoDbClient = require("mongodb").MongoClient
-const mongoURI = 'mongodb+srv://tomato:otamot@cluster0.qwobysp.mongodb.net/tomato?retryWrites=true&w=majority'
+const mongoURI = 'mongodb+srv://tomato:otamot@cluster0.qwobysp.mongodb.net/CheesyChuckles?retryWrites=true&w=majority'
 // mongodb://<username>:<password>@merncluster-shard-00-00.d1d4z.mongodb.net:27017,merncluster-shard-00-01.d1d4z.mongodb.net:27017,merncluster-shard-00-02.d1d4z.mongodb.net:27017/?ssl=true&replicaSet=atlas-eusy5p-shard-0&authSource=admin&retryWrites=true&w=majority
 module.exports = function (callback) {
     mongoose.connect(mongoURI, { useNewUrlParser: true }, async (err, result) => {
